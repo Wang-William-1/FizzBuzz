a3cc9d42a6aebd3f4ac17b3f9b3bd2cd16d224ce
William-Wang-1 <jinglin.wang1995@gmail.com>
2024-09-09 10:54:30 -0400

    added Multiples and Reduce class

---
+++ src/Reduce.java
@@ -0,0 +1,21 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int n = 100;
+        int steps = 0;
+
+        while (n != 0) {
+
+            Boolean even = n % 2 == 0;
+
+            if (even) {
+                n /= 2;
+            }
+            else {
+                n -= 1;
+            }
+            steps++;
+        }
+        System.out.println(steps + " steps");
+    }
+}
